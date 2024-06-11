71fcf145bf0568fcfa1864cc91b49198b517fc34
Viet <Vinh@VIET>
2024-06-11 12:59:36 +0700

    draw map 3

---
+++ Resource Files/ContraMapStage3BG_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="ContraMapStage3BG" tilewidth="16" tileheight="16" tilecount="2656" columns="16">
+ <image source="ContraMapStage3BG.png" width="256" height="2656"/>
+</tileset>
